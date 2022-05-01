3ea266fcd074fb2a9464dfa074efc583ed7410a5
Anthony Salas <anthony.salas@tecsup.edu.pe>
2022-05-01 00:47:05 -0500

    lab07

---
+++ intro.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="main">
+        <nav class="navbar navbar-default" style="display: inline-block;">
+          <h3 style="color: white; position: relative;">CLOUD COMPUTING 5to</h3>
+          <h4 style="color: white; position: relative;">TECSUP</h4>
+          <ul class="nav navbar-nav">
+            <li><a href="#0">LAB01</a></li>
+            <li><a href="#0">LAB02</a></li>
+            <li><a href="#0">LAB07</a></li>
+            <li class="dropdown">
+              <a href="#0" class="dropdown-toggle" data-toggle="dropdown" data-target="dropdown" role="button" aria-haspopup="true" aria-expanded="false">LAB03<span class="caret"></span></a>
+              <ul class="dropdown-menu" id="dropdon-menu">
+                <li><a href="#0">Burger King</a></li>
+                <li><a href="#0">Southwest Airlines</a></li>
+                <li><a href="#0">Levi Strauss</a></li>
+              </ul>
+            </li>
+            <li><a href="#0">LAB04</a></li>
+          </ul>
+        </nav>
+        </div>
+        <script src="main.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+</html>
